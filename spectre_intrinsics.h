d720f5a943fe5597005801601fc42b6aad721c8a
Takashi Yoshi <takashi@yoshi.email>
2018-01-08 12:18:13 +0100

    Add intrinsics for PowerPC (if Apple headers are missing)

--- spectre_intrinsics.h
+++ spectre_intrinsics.h
@@ -8,7 +8,18 @@
 #if defined(__APPLE__)
 #include <ppc_intrinsics.h>
 #else
+#define __SPR_TBL       268             /* Time-base Lower. Not on PPC 601 */
+#define __SPR_TBU       269             /* Time-base Upper. Not on PPC 601 */
 
+#define __mfspr(spr)    \
+  __extension__ ({ long mfsprResult; \
+     __asm__ volatile ("mfspr %0, %1" : "=r" (mfsprResult) : "n" (spr)); \
+     /*return*/ mfsprResult; })
+
+#define __dcbf(base, index)     \
+  __asm__ ("dcbf %0, %1" : /*no result*/ : "b%" (index), "r" (base) : "memory")
+#define __mftb()                __mfspr(__SPR_TBL)
+#define __mftbu()               __mfspr(__SPR_TBU)
 #endif
 
 #elif defined(__ARCH_ARM__)
