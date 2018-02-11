f4c415da46ecb5253334c697a09ecf5c1eeccab5
Takashi Yoshi <takashi@yoshi.email>
2018-02-11 10:30:42 +0100

    Implement pointer chasing cache_timer

--- cache_timer.c
+++ cache_timer.c
@@ -6,39 +6,45 @@
 #include "spectre_intrinsics.h"
 #include "timer.h"
 
-#define NUM_ROUNDS 32
+#define NUM_ROUNDS 4
+#define CACHE_LINE_LEN 16
 
 unsigned int _value = 1; /* just some value */
 
 int main(void) {
-	unsigned int i, junk;
+	unsigned int i, j;
+	uint32_t *values[CACHE_LINE_LEN];
 	register uint64_t time1, time2;
-	volatile unsigned int *_value_addr;
+	volatile uint32_t *value_ptr;
 
-	_value_addr = &_value;
-
-	/* preload value */
-	junk = *_value_addr;
+	/* fill values with pointers */
+	for (i = 0; i < (CACHE_LINE_LEN - 1); ++i) {
+		values[i] = (uint32_t *)&(values[i+1]);
+	}
+	values[(CACHE_LINE_LEN - 1)] = (uint32_t *)&(values[0]);
 
 	uint64_t sum = 0;
 
 	for (i = 0; i < NUM_ROUNDS; ++i) {
-		/* read timer (start) */
-		time1 = __gettime();
+		value_ptr = (uint32_t *)&values;
+		for (j = 0; j < CACHE_LINE_LEN; ++j) {
+			/* read timer (start) */
+			time1 = __gettime();
 
-		/* memory access to time */
-		junk = *_value_addr; 
+			/* memory access to time */
+			value_ptr = (uint32_t *)(*value_ptr);
 
-		/* read timer (end) */
-		time2 = __gettime();
+			/* read timer (end) */
+			time2 = __gettime();
 
-		/* print duration */
-		unsigned int duration = (time2 - time1);
-		sum += duration;
-		printf("%3u: %u\n", i, duration);
+			/* print duration */
+			unsigned int duration = (time2 - time1);
+			sum += duration;
+			printf("%3u,%3u: %u\n", i, j, duration);
+		}
 	}
 
-	printf("\nAvg: %.2f\n", (double)(sum / NUM_ROUNDS));
+	printf("\nAvg: %.2f\n", (double)(sum / NUM_ROUNDS / CACHE_LINE_LEN));
 
 	return EXIT_SUCCESS;
 }
