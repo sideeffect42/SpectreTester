5b0ef049c0ce55b7bc61a1107847f60a2259a759
Takashi Yoshi <takashi@yoshi.email>
2018-02-11 10:31:42 +0100

    Add 32/64-bit macro for architectures

--- spectre_archs.h
+++ spectre_archs.h
@@ -4,15 +4,37 @@
 #if defined(__powerpc__) || defined(__ppc__)  || defined(__ppc64__)
 // PowerPC
 #define __ARCH_POWERPC__
+#if defined(__ppc64__)
+#define __ARCH_64BIT__ 1
+#define __ARCH_32BIT__ 0
+#else
+#define __ARCH_32BIT__ 1
+#define __ARCH_64BIT__ 0
+#endif
 #elif defined(__arm__) || defined(__thumb__) || defined(__aarch64__)
 // ARM
 #define __ARCH_ARM__
+#if defined(__aarch64__)
+#define __ARCH_64BIT__ 1
+#define __ARCH_32BIT__ 0
+#else
+#define __ARCH_32BIT__ 1
+#define __ARCH_64BIT__ 0
+#endif
 #elif defined(__s390x__) || defined(__zarch__) || defined(__s390__) || defined(__SYSC_ZARCH__)
 // s390x
 #define __ARCH_S390X__
 #elif defined(__amd64__) || defined(__x86_64__) || defined(_M_X64) || defined(_M_AMD64) || defined(__i386__) || defined(_M_IX86) || defined(__X86__) || defined(__INTEL__) || defined(_X86_)
 // x86
 #define __ARCH_X86__
+#if defined(__amd64__) || defined(__x86_64__) || defined(_M_X64) || defined(_M_AMD64)
+#define __ARCH_64BIT__ 1
+#define __ARCH_32BIT__ 0
+#else
+#define __ARCH_32BIT__ 1
+#define __ARCH_64BIT__ 0
+#endif
+
 #else
 
 // Unsupported CPU architecture
